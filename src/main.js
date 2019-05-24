15d90e6832318dbd3480cf1c746c7d46cdb29df8
lebrancconvas <lebranccconvas@gmail.com>
2019-05-24 09:06:48 +0700

    Use Bootstrap-Vue.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
