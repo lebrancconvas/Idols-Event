11a93c747e54f8065f19b3c82d5bbb4e2444970d
lebrancconvas <lebranccconvas@gmail.com>
2019-05-27 11:09:42 +0700

    Create API Component.

---
+++ src/components/Api.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        {{info}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Api",
+    data() {
+        return {
+            info: ''
+        }
+    },
+    mounted() {
+        this.$http
+            .get('https://opener.now.sh/api/data/234567')
+            .then(response => this.info = JSON.stringify(response))
+    }
+}
+</script>
+
+<style lang="css">
+    
+</style>
